1488fab411021b8b18194b0e57cc86f651274f33
YazawaKenichi <tonfarken83825@gmail.com>
2025-12-10 19:44:01 +0900

    micro-ROS と uart を同時に使うテスト （stepper_subscriber を executor に入れてなかったのが原因なのではと気づいた瞬間）

--- src/ros2.c
+++ src/ros2.c
@@ -53,76 +53,15 @@ rcl_ret_t ros2_generate_node()
 
 rcl_ret_t ros2_generate_subscriber()
 {
-    //! ステッピングモータのサブスクライバ
     rcl_ret_t rc;
-    rc = rclc_subscription_init_default(&stepper_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/stepper/position/raw");
-    rclc_subscription_init_default(&loading_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/loading/deg");
-    rclc_executor_add_subscription(&executor_, &loading_subscriber_, &loading_msg_, loading_callback_, ON_NEW_DATA);
-
-    ///// サブスクライバの作成 /////
-    //! 砲台左のサブスクライバ
-    rclc_subscription_init_default(&gun_l_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/gun/left/pwm/duty");
-    rclc_executor_add_subscription(&executor_, &gun_l_subscriber_, &gun_l_msg_, gun_l_callback_, ON_NEW_DATA);
-    //! 砲台右のサブスクライバ
-    rclc_subscription_init_default(&gun_r_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/gun/right/pwm/duty");
-    rclc_executor_add_subscription(&executor_, &gun_r_subscriber_, &gun_r_msg_, gun_r_callback_, ON_NEW_DATA);
-    //! 砲塔サーボのサブスクライバ
-#if INTEGRATE
-    rclc_subscription_init_default(&pose_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3), "/pico/pose");
-    rclc_executor_add_subscription(&executor_, &pose_subscriber_, &pose_msg_, pose_callback_, ON_NEW_DATA);
-#else
-    rclc_subscription_init_default(&roll_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/roll/deg");
-    rclc_executor_add_subscription(&executor_, &roll_subscriber_, &roll_msg_, roll_callback_, ON_NEW_DATA);
-    rclc_subscription_init_default(&pitch_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/pitch/deg");
-    rclc_executor_add_subscription(&executor_, &pitch_subscriber_, &pitch_msg_, pitch_callback_, ON_NEW_DATA);
-    rclc_subscription_init_default(&yaw_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/yaw/deg");
-    rclc_executor_add_subscription(&executor_, &yaw_subscriber_, &yaw_msg_, yaw_callback_, ON_NEW_DATA);
-#endif
-    rclc_executor_add_subscription(&executor_, &stepper_subscriber_, &stepper_msg_, stepper_callback_, ON_NEW_DATA);
+    rc = rclc_subscription_init_default(&test_subscriber_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/test");
+    rclc_executor_add_subscription(&executor_, &test_subscriber_, &test_msg_, test_callback_, ON_NEW_DATA);
     return rc;
 }
 
 rcl_ret_t ros2_generate_publisher()
 {
     rcl_ret_t rc;
-#define PUBLISHER 0
-#if PUBLISHER
-    rcl_timer_t gun_l_timer_;
-    rclc_publisher_init_default(&gun_l_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/gun/left/pwm/level");
-    rclc_timer_init_default(&gun_l_timer_, &support_, RCL_MS_TO_NS(1000), gun_l_timer_callback_);
-    rclc_executor_add_timer(&executor_, &gun_l_timer_);
-    rcl_timer_t gun_r_timer_;
-    rclc_publisher_init_default(&gun_r_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/gun/right/pwm/level");
-    rclc_timer_init_default(&gun_r_timer_, &support_, RCL_MS_TO_NS(1000), gun_r_timer_callback_);
-    rclc_executor_add_timer(&executor_, &gun_r_timer_);
-    rcl_timer_t pose_timer_;
-    rcl_timer_t roll_timer_;
-    rcl_timer_t pitch_timer_;
-    rcl_timer_t yaw_timer_;
-    rcl_timer_t loading_timer_;
-#if INTEGRATE
-    rclc_publisher_init_default(&pose_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/pose/level");
-    rclc_timer_init_default(&pose_timer_, &support_, RCL_MS_TO_NS(1000), pose_timer_callback_);
-    rclc_executor_add_timer(&executor_, &pose_timer_);
-#else
-    rclc_publisher_init_default(&roll_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/roll/level");
-    rclc_timer_init_default(&roll_timer_, &support_, RCL_MS_TO_NS(1000), roll_timer_callback_);
-    rclc_executor_add_timer(&executor_, &roll_timer_);
-    rclc_publisher_init_default(&pitch_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/pitch/level");
-    rclc_timer_init_default(&pitch_timer_, &support_, RCL_MS_TO_NS(1000), pitch_timer_callback_);
-    rclc_executor_add_timer(&executor_, &pitch_timer_);
-    rclc_publisher_init_default(&yaw_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/yaw/level");
-    rclc_timer_init_default(&yaw_timer_, &support_, RCL_MS_TO_NS(1000), yaw_timer_callback_);
-    rclc_executor_add_timer(&executor_, &yaw_timer_);
-#endif //! INTEGRATE
-    rclc_publisher_init_default(&loading_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/pico/loading/level");
-    rclc_timer_init_default(&loading_timer_, &support_, RCL_MS_TO_NS(1000), loading_timer_callback_);
-    rclc_executor_add_timer(&executor_, &loading_timer_);
-    rcl_timer_t stepper_timer_;
-    rclc_publisher_init_default(&stepper_publisher_, &node_, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32), "/pico/stepper/position/debug");
-    rclc_timer_init_default(&stepper_timer_, &support_, RCL_MS_TO_NS(1000), stepper_timer_callback_);
-    rclc_executor_add_timer(&executor_, &stepper_timer_);
-#endif //! PUBLISHER
     return rc;
 }
 
