3b51cf99ff8832238dbe0a99734657f7c16eee0d
YazawaKenichi <tonfarken83825@gmail.com>
2025-12-10 19:16:14 +0900

    UART ピン変更

--- include/pico-pwm/preload.h
+++ include/pico-pwm/preload.h
@@ -2,19 +2,19 @@
 #define __PRELOAD_H__
 
 ///// PIN 設定 /////
-#define GUN_L_PIN 0     //! GPIO0
-#define GUN_R_PIN 1     //! GPIO1
+#define GUN_L_PIN 0
+#define GUN_R_PIN 1
 
-#define ROLL_PIN 2      //! GPIO2
-#define PITCH_PIN 3     //! GPIO3
+#define ROLL_PIN 2
+#define PITCH_PIN 3
 
-#define YAW_PIN 5       //! GPIO4
-#define LOADING_PIN 6   //! GPIO5
+#define YAW_PIN 5
+#define LOADING_PIN 6
 
-#define STEPPER_PIN 9   //! GPIO6
+#define STEPPER_PIN 9
 
-#define UART_TX_PIN 21  //! GPIO21
-#define UART_RX_PIN 22  //! GPIO22
+#define UART_TX_PIN 16
+#define UART_RX_PIN 17
 
 #define RESCALE(X, I_MAX, I_MIN, O_MAX, O_MIN) ((O_MAX - O_MIN) * (float) ((X - I_MIN) / (float) (I_MAX - I_MIN)) + O_MIN)
 #define HZ2US(HERTZ) (1000 * 1000 * 1 / ((float) HERTZ))
