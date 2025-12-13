675e5768536d5ef45cf4075cd1ac020078afe60b
YazawaKenichi <tonfarken83825@gmail.com>
2025-12-13 13:22:35 +0900

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -9,9 +9,11 @@ make -j4
 PICO_DIR="/media/yazawa/RPI-RP2"
 
 while [ ! -d "$PICO_DIR" ] ; do
-    echo "Pico が書き込みモードで接続されていません\r"
+    echo -e "Pico が書き込みモードで接続されていません\r"
     sleep 1
 done
+
+echo "Done!"
 
 cp *.uf2 /media/yazawa/RPI-RP2
 
